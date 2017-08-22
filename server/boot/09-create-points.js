7cf6c17c6dfe7469afef4393942946c53bf9eb34
Guillermo <guillermo.riverola@outlook.com>
2017-08-22 16:29:56 +0200

    badge

--- server/boot/09-create-points.js
+++ server/boot/09-create-points.js
@@ -40,7 +40,7 @@ module.exports = function (app, cb) {
   },{
     name: 'Retraso',
     image: 'https://previews.123rf.com/images/alexmillos/alexmillos1203/alexmillos120300047/12950777-cuadro-de-retraso-en-la-entrega-y-el-dise-o-de-la-ilustraci-n-reloj-en-blanco-Foto-de-archivo.jpg',
-    value: '2',
+    value: '-2',
     id: 100005,  
     schoolId: 1,
     teacherId: 1000
@@ -59,7 +59,7 @@ module.exports = function (app, cb) {
 		value: '1',
 		pointId: 100000,
 		groupId: 1,
-		studentId: 10001,
+		studentId: 10000,
 		schoolId: 1
 	  }, {
 		id: 3,
@@ -71,9 +71,254 @@ module.exports = function (app, cb) {
 	  }, {
 		id: 4,
 		value: '1',
+		pointId: 100001,
+		groupId: 1,
+		studentId: 10000,
+		schoolId: 1
+	  }, {
+		id: 5,
+		value: '1',
+		pointId: 100001,
+		groupId: 1,
+		studentId: 10000,
+		schoolId: 1
+	  }, {
+		id: 6,
+		value: '1',
+		pointId: 100002,
+		groupId: 1,
+		studentId: 10000,
+		schoolId: 1
+	  }, {
+		id: 7,
+		value: '1',
+		pointId: 100002,
+		groupId: 1,
+		studentId: 10000,
+		schoolId: 1
+	  }, {
+		id: 8,
+		value: '1',
+		pointId: 100002,
+		groupId: 2,
+		studentId: 10000,
+		schoolId: 1
+	  }, {
+		id: 9,
+		value: '1',
+		pointId: 100002,
+		groupId: 1,
+		studentId: 10000,
+		schoolId: 1
+	  }, {
+		id: 10,
+		value: '1',
+		pointId: 100002,
+		groupId: 1,
+		studentId: 10002,
+		schoolId: 1
+	  }, {
+		id: 11,
+		value: '1',
 		pointId: 100000,
 		groupId: 1,
 		studentId: 10000,
+		schoolId: 1
+	  }, {
+		id: 12,
+		value: '1',
+		pointId: 100000,
+		groupId: 1,
+		studentId: 10000,
+		schoolId: 1
+	  }, {
+		id: 13,
+		value: '1',
+		pointId: 100001,
+		groupId: 1,
+		studentId: 10000,
+		schoolId: 1
+	  }, {
+		id: 14,
+		value: '1',
+		pointId: 100001,
+		groupId: 1,
+		studentId: 10000,
+		schoolId: 1
+	  }, {
+		id: 15,
+		value: '1',
+		pointId: 100002,
+		groupId: 1,
+		studentId: 10000,
+		schoolId: 1
+	  },{
+		id: 16,
+		value: '1',
+		pointId: 100002,
+		groupId: 1,
+		studentId: 10000,
+		schoolId: 1
+	  },{
+		id: 17,
+		value: '1',
+		pointId: 100005,
+		groupId: 1,
+		studentId: 10000,
+		schoolId: 1
+	  },{
+		id: 18,
+		value: '1',
+		pointId: 100002,
+		groupId: 2,
+		studentId: 10000,
+		schoolId: 1
+	  }, {
+		id: 19,
+		value: '1',
+		pointId: 100002,
+		groupId: 2,
+		studentId: 10000,
+		schoolId: 1
+	  }, {
+		id: 20,
+		value: '1',
+		pointId: 100002,
+		groupId: 2,
+		studentId: 10008,
+		schoolId: 1
+	  }, {
+		id: 21,
+		value: '1',
+		pointId: 100000,
+		groupId: 2,
+		studentId: 10000,
+		schoolId: 1
+	  }, {
+		id: 22,
+		value: '1',
+		pointId: 100000,
+		groupId: 2,
+		studentId: 10000,
+		schoolId: 1
+	  }, {
+		id: 23,
+		value: '1',
+		pointId: 100001,
+		groupId: 2,
+		studentId: 10000,
+		schoolId: 1
+	  }, {
+		id: 24,
+		value: '1',
+		pointId: 100001,
+		groupId: 3,
+		studentId: 10008,
+		schoolId: 1
+	  }, {
+		id: 25,
+		value: '1',
+		pointId: 100002,
+		groupId: 3,
+		studentId: 10008,
+		schoolId: 1
+	  },{
+		id: 26,
+		value: '1',
+		pointId: 100002,
+		groupId: 3,
+		studentId: 10008,
+		schoolId: 1
+	  },{
+		id: 27,
+		value: '1',
+		pointId: 100005,
+		groupId: 3,
+		studentId: 10004,
+		schoolId: 1
+	  }, {
+		id: 28,
+		value: '1',
+		pointId: 100000,
+		groupId: 3,
+		studentId: 10008,
+		schoolId: 1
+	  },{
+		id: 29,
+		value: '1',
+		pointId: 100002,
+		groupId: 3,
+		studentId: 10008,
+		schoolId: 1
+	  },{
+		id: 30,
+		value: '1',
+		pointId: 100002,
+		groupId: 3,
+		studentId: 10004,
+		schoolId: 1
+	  },{
+		id: 31,
+		value: '1',
+		pointId: 100000,
+		groupId: 3,
+		studentId: 10000,
+		schoolId: 1
+	  }, {
+		id: 32,
+		value: '1',
+		pointId: 100000,
+		groupId: 3,
+		studentId: 10000,
+		schoolId: 1
+	  }, {
+		id: 33,
+		value: '1',
+		pointId: 100001,
+		groupId: 3,
+		studentId: 10000,
+		schoolId: 1
+	  }, {
+		id: 34,
+		value: '1',
+		pointId: 100001,
+		groupId: 2,
+		studentId: 10008,
+		schoolId: 1
+	  }, {
+		id: 35,
+		value: '1',
+		pointId: 100002,
+		groupId: 2,
+		studentId: 10008,
+		schoolId: 1
+	  },{
+		id: 36,
+		value: '1',
+		pointId: 100002,
+		groupId: 2,
+		studentId: 10008,
+		schoolId: 1
+	  },{
+		id: 37,
+		value: '1',
+		pointId: 100001,
+		groupId: 3,
+		studentId: 10004,
+		schoolId: 1
+	  },{
+		id: 38,
+		value: '1',
+		pointId: 100005,
+		groupId: 1,
+		studentId: 10008,
+		schoolId: 1
+	  },{
+		id: 39,
+		value: '1',
+		pointId: 100001,
+		groupId: 1,
+		studentId: 10008,
 		schoolId: 1
 	  }], function (err) {
 	
@@ -81,3 +326,4 @@ module.exports = function (app, cb) {
 	});
   });
 };
+
